ab3a6b4af42dd5db0c1d126f967e025eea5c75c0
Francisco <Francisco@192.168.2.8>
2014-03-06 23:06:43 -0300

    Commit #1

---
+++ Tesis/src/cl/rojasycia/tesisguiamovil/interfacesusuario/Fragment1.java
@@ -0,0 +1,19 @@
+package cl.rojasycia.tesisguiamovil.interfacesusuario;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.androidbegin.sidemenututorial.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends SherlockFragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment1, container, false);
+		return rootView;
+	}
+
+}
---
+++ Tesis/src/cl/rojasycia/tesisguiamovil/interfacesusuario/SplashScreen.java
@@ -0,0 +1,41 @@
+package cl.rojasycia.tesisguiamovil.interfacesusuario;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.androidbegin.sidemenututorial.R;
+
+public class SplashScreen extends SherlockActivity {
+
+	// Splash screen timer
+	private static int SPLASH_TIME_OUT = 2000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		getSupportActionBar().hide();
+
+		new Handler().postDelayed(new Runnable() {
+
+			/*
+			 * Showing splash screen with a timer. This will be useful when you
+			 * want to show case your app logo / company
+			 */
+
+			@Override
+			public void run() {
+				// This method will be executed once the timer is over
+				// Start your app main activity
+				Intent i = new Intent(SplashScreen.this, PrincipalActivity.class);
+				startActivity(i);
+
+				// close this activity
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+
+}
