9386bdadc0250dcded9d8ae18fcbc877ba7054f5
Francisco <Francisco@192.168.2.8>
2014-03-08 11:52:14 -0300

    Commit #10

--- Tesis/src/cl/rojasycia/tesisguiamovil/interfacesusuario/Fragment2.java
+++ Tesis/src/cl/rojasycia/tesisguiamovil/interfacesusuario/Fragment2.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+//dfxgbfd
 public class Fragment2 extends SherlockFragment {
 	
 	private ArrayList<UniversidadItem> arrayUniversidades;
@@ -29,16 +30,15 @@ public class Fragment2 extends SherlockFragment {
 		adapter = new UniversidadAdapter(container.getContext(), arrayUniversidades);
 		lvlUniversidades.setAdapter(adapter);
 
-		
 		return rootView;
 	}
 	
 
 	private void rellenarArrayList() {
-		arrayUniversidades.add(new UniversidadItem("Universidad de Playa Ancha de Ciencias de la Educación", R.drawable.u_upla));
-		arrayUniversidades.add(new UniversidadItem("Pontificia Universidad Católica de Valparaiso", R.drawable.u_pucv));
-		arrayUniversidades.add(new UniversidadItem("Universidad Técnica Federico Santa María", R.drawable.u_usm));
-		arrayUniversidades.add(new UniversidadItem("Universidad de Valparaiso", R.drawable.u_uv));
+//		arrayUniversidades.add(new UniversidadItem("Universidad de Playa Ancha de Ciencias de la Educación", R.drawable.u_upla));
+//		arrayUniversidades.add(new UniversidadItem("Pontificia Universidad Católica de Valparaiso", R.drawable.u_pucv));
+//		arrayUniversidades.add(new UniversidadItem("Universidad Técnica Federico Santa María", R.drawable.u_usm));
+//		arrayUniversidades.add(new UniversidadItem("Universidad de Valparaiso", R.drawable.u_uv));
 	}
 
 }
