76af90d2809416a4be2848b8cb7cc1796c50cf48
Francisco <pnxo0o@gmail.com>
2014-04-08 15:02:53 -0300

    Clases necesarias

---
+++ Tesis/src/cl/rojasycia/tesisguiamovil/ui/NoticiasActivity.java
@@ -0,0 +1,7 @@
+package cl.rojasycia.tesisguiamovil.ui;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class NoticiasActivity extends SherlockActivity {
+
+}
---
+++ Tesis/src/cl/rojasycia/tesisguiamovil/ui/POIActivity.java
@@ -0,0 +1,7 @@
+package cl.rojasycia.tesisguiamovil.ui;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class POIActivity extends SherlockActivity {
+
+}
