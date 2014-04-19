5aeb3007b1f5dfb2890729d9fda5ede3c8d4fff3
Francisco <pnxo0o@gmail.com>
2014-04-19 20:26:03 -0300

    probando conexion a internet

--- Tesis/src/cl/rojasycia/tesisguiamovil/ui/Fragment1.java
+++ Tesis/src/cl/rojasycia/tesisguiamovil/ui/Fragment1.java
@@ -5,30 +5,49 @@ import com.actionbarsherlock.app.SherlockFragment;
 import cl.rojasycia.tesisguiamovil.R;
 import cl.rojasycia.tesisguiamovil.struct.ListViewExpanableAdaptador;
 import cl.rojasycia.tesisguiamovil.struct.ListViewExpanableItems;
+import cl.rojasycia.tesisguiamovil.utils.NetworkUtil;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 public class Fragment1 extends SherlockFragment {
 	
 	SparseArray<ListViewExpanableItems> grupos = new SparseArray<ListViewExpanableItems>();
+	Button btnBuscarAqui;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment1, container, false);
+		
 		crearDatos();
-		ExpandableListView listView = (ExpandableListView) rootView.findViewById(R.id.listViewexp);
+		ExpandableListView listaExpandible = (ExpandableListView) rootView.findViewById(R.id.listViewexp);
 		ListViewExpanableAdaptador adapter = new ListViewExpanableAdaptador(getActivity(), grupos);
-		listView.setAdapter(adapter);
+		listaExpandible.setAdapter(adapter);
+		
+		btnBuscarAqui = (Button) rootView.findViewById(R.id.btnBuscarAqui);
+		btnBuscarAqui.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v)
+				{
+			    	buscarAqui();
+			    } 
+			}); 
+		
 		return rootView;
 	}
 	
+	
+	/**
+	 * Método que crea los datos de la lista expandible
+	 */
 	public void crearDatos() {
-
 		ListViewExpanableItems grupo0 = new ListViewExpanableItems("Universidades");
 		grupo0.children.add("Universidad de Playa Ancha de Ciencias de la Educación");
 		grupo0.children.add("Universidad de Valparaíso");
@@ -53,4 +72,12 @@ public class Fragment1 extends SherlockFragment {
 		grupos.append(4, grupo4);
 	}
 
+	/**
+	 * @param view
+	 * Método que valida la conexión y ubicación
+	 * y busca puntos de interes
+	 */
+	public void buscarAqui() {
+        Toast.makeText(getActivity(), NetworkUtil.getConnectivityStatus(getActivity()), Toast.LENGTH_SHORT).show();
+    }
 }
