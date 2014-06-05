5a946b0921b8e1bf59d5ddc06d0b3a5b50d4f9ae
Francisco <pnxo0o@gmail.com>
2014-06-05 18:04:38 -0400

    iconos nuevos

---
+++ Tesis/src/cl/rojasycia/tesisguiamovil/ui/Fragment4.java
@@ -0,0 +1,20 @@
+package cl.rojasycia.tesisguiamovil.ui;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+import cl.rojasycia.tesisguiamovil.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment4 extends SherlockFragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment4, container, false);
+		return rootView;
+	}
+
+}
